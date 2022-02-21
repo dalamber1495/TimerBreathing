128187ede73e5b4f4325170bb57e594d3643c39a
Rodion <revan1495@mail.ru>
2022-02-21 15:23:19 +0700

    MVVM+UseCase+NumberPicker without UI, Navigation, Metronome

--- app/src/main/java/com/example/timerbreathing/presentation/fragments/BreathFragment.kt
+++ app/src/main/java/com/example/timerbreathing/presentation/fragments/BreathFragment.kt
@@ -9,6 +9,7 @@ import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +17,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.timerbreathing.R
 import com.example.timerbreathing.data.ExerciseParameters
 import com.example.timerbreathing.databinding.FragmentBreathBinding
+import com.example.timerbreathing.other.Constants
 import com.example.timerbreathing.presentation.TimerState
 import com.example.timerbreathing.presentation.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,34 +34,46 @@ class BreathFragment : Fragment(R.layout.fragment_breath) {
         binding = FragmentBreathBinding.bind(view)
         viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
 
-        binding.progressBar.max = viewModel.userDataParameters.timeTraining.toInt()
-
         binding.timeExerciseTv.setOnClickListener {
-            findNavController().navigate(R.id.action_breathFragment_to_pickerDialogFragment)
+            openPicker(R.id.time_exercise_tv)
+        }
+        binding.timeBreathBtn.setOnClickListener {
+            openPicker(R.id.time_breath_btn)
+        }
+        binding.timeWaitBtn.setOnClickListener {
+            openPicker(R.id.time_wait_btn)
+        }
+        binding.timeExhBtn.setOnClickListener {
+            openPicker(R.id.time_exh_btn)
+        }
+        binding.timeWait2Btn.setOnClickListener {
+            openPicker(R.id.time_wait2_btn)
         }
         binding.breathBtn.setOnClickListener { v ->
-            when (viewModel.curTimeBreath.value) {
-                is TimerState.Started -> {
-                    (v as TextView).text = getString(R.string.breath)
-                }
-                is TimerState.Stopped -> {
-                    (v as TextView).text = getString(R.string.stop_timer)
-
-                }
-                else -> {}
-            }
+//            when (viewModel.curTimeBreath.value) {
+//                is TimerState.Started -> {
+//                    (v as TextView).text = getString(R.string.breath)
+//                }
+//                is TimerState.Stopped -> {
+//                    (v as TextView).text = getString(R.string.stop_timer)
+//                }
+//                else -> {}
+//            }
             //Здесь отправляются параметры упражнения
-            viewModel.startTimer(dataTime = ExerciseParameters())
+            viewModel.startTimer()
         }
 
         viewModel.curTimeBreath.observe(viewLifecycleOwner) {
             when (it) {
                 is TimerState.Stopped -> {
                     viewModel.timer?.cancel()
+                    binding.breathBtn.text = getString(R.string.breathing)
                     updateCountdownUI(it)
 
                 }
                 is TimerState.Started -> {
+                    binding.progressBar.max = viewModel.immutableParameters.timeTraining.toInt()
+                    binding.breathBtn.text = getString(R.string.stop_timer)
                     updateCountdownUI(it)
                 }
             }
@@ -84,10 +98,14 @@ class BreathFragment : Fragment(R.layout.fragment_breath) {
             timeExhBtn.text = timeState.dataTime.timeExhalation.toString()
             timeWait2Btn.text = timeState.dataTime.timeExhalationDelay.toString()
             progressBar.progress =
-                (viewModel.userDataParameters.timeTraining - timeState.dataTime.timeTraining).toInt()
+                (viewModel.immutableParameters.timeTraining - timeState.dataTime.timeTraining).toInt()
             Log.e("TAG", "MAX: ${progressBar.max}")
             Log.e("TAG", "PROGRESS: ${progressBar.progress}")
 
         }
+    }
+    private fun openPicker(resValue:Int){
+        findNavController().navigate(R.id.action_breathFragment_to_pickerDialogFragment,
+            bundleOf(Pair(Constants.RESOURCE,resValue)))
     }
 }
--- app/src/main/java/com/example/timerbreathing/presentation/fragments/PickerDialogFragment.kt
+++ app/src/main/java/com/example/timerbreathing/presentation/fragments/PickerDialogFragment.kt
@@ -9,21 +9,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.timerbreathing.R
+import com.example.timerbreathing.databinding.FragmentPickerDialogBinding
+import com.example.timerbreathing.other.Constants
+import com.example.timerbreathing.presentation.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class PickerDialogFragment : DialogFragment(R.layout.fragment_picker_dialog) {
 
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
- //       dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        //       dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         return super.onCreateView(inflater, container, savedInstanceState)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentPickerDialogBinding.bind(view)
+        val resValue = arguments?.get(Constants.RESOURCE) as Int
+        binding.apply {
+            numberPicker.maxValue = 10
+            numberPicker.minValue = 1
+            numberPicker.setOnValueChangedListener { _, _, i2 ->
+                viewModel.changeParameters(resValue, i2)
+            }
+
+        }
+    }
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return super.onCreateDialog(savedInstanceState)
