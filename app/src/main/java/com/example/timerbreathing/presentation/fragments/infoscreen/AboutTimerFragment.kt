ac0ddb9d047b509d36f09f18ceb8ad72b64251e4
Rodion <revan1495@mail.ru>
2022-03-02 17:06:16 +0700

    MVVM+UseCase+NumberPicker+Metronome+ UI + Navigation

---
+++ app/src/main/java/com/example/timerbreathing/presentation/fragments/infoscreen/AboutTimerFragment.kt
@@ -0,0 +1,27 @@
+package com.example.timerbreathing.presentation.fragments.infoscreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.core.os.bundleOf
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.timerbreathing.R
+import com.example.timerbreathing.databinding.FragmentAboutTimerBinding
+import com.example.timerbreathing.presentation.fragments.ARG_STARTED
+import com.example.timerbreathing.presentation.viewmodels.MainViewModel
+
+class AboutTimerFragment : Fragment(R.layout.fragment_about_timer) {
+    private lateinit var binding: FragmentAboutTimerBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAboutTimerBinding.bind(view)
+        binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
+        binding.relaxBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_aboutTimerFragment_to_breathFragment)
+        }
+        binding.schemeBtn.setOnClickListener { findNavController().navigate(R.id.action_aboutTimerFragment_to_schemeBreathFragment) }
+    }
+
+}
---
+++ app/src/main/java/com/example/timerbreathing/presentation/fragments/infoscreen/schemescreen/CheerfulnessFragment.kt
@@ -0,0 +1,22 @@
+package com.example.timerbreathing.presentation.fragments.infoscreen.schemescreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.timerbreathing.R
+import com.example.timerbreathing.databinding.FragmentCheerfulnessBinding
+import com.example.timerbreathing.presentation.viewmodels.MainViewModel
+
+class CheerfulnessFragment : Fragment(R.layout.fragment_cheerfulness) {
+    private lateinit var binding: FragmentCheerfulnessBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCheerfulnessBinding.bind(view)
+        binding.toolbar.setNavigationOnClickListener {findNavController().popBackStack() }
+        binding.relaxBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_cheerfulnessFragment_to_breathFragment) }
+    }
+}
